6cd2004bb86fe963e89a31a56ce289fbb605b167
talesmig <tales.mig@hotmail.com>
2022-06-12 21:25:07 -0300

    criação dos botões que “pulam” para as jogadas anteriores

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -79,6 +79,17 @@ class Game extends React.Component {
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
+        
+        const moves = history.map((step, move) => {
+            const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
 
         return (
             <div className="game">
@@ -90,7 +101,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
